4cec5b08c2100381c37f5765c6c39d2a2005644a
jin1401 <seongjin8247@gmail.com>
2022-12-06 16:50:15 +0100

    Add files via upload
    add event listner, get element by id

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,25 @@
-function seleccionarMascotaJugador(){
-    alert('Seleccionaste tu mascota')
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if(inputHipodoge.checked){
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked){
+        alert('Seleccionaste a Capipepo')
+    }
+    else if (inputRatigueya.checked){
+        alert('Seleccionaste a Ratigueya')
+    } else {
+        alert('Seleccionar una mascota')
+    }
+}
+
+
+
+window.addEventListener('load', iniciarJuego)
