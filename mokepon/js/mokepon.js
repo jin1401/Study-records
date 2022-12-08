38d72e86b8558749ea412d9d80ae370e933d71ae
jin1401 <seongjin8247@gmail.com>
2022-12-08 23:34:11 +0100

    Add files via upload
    learned innerHTML

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -7,14 +7,15 @@ function seleccionarMascotaJugador(){
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if(inputHipodoge.checked){
-        alert('Seleccionaste a Hipodoge')
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (inputCapipepo.checked){
-        alert('Seleccionaste a Capipepo')
+        spanMascotaJugador.innerHTML = 'Capipepo'
     }
     else if (inputRatigueya.checked){
-        alert('Seleccionaste a Ratigueya')
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     } else {
         alert('Seleccionar una mascota')
     }
