4e4a87afc4fdeb7e088d6874dede86bd7f3fcd76
jin1401 <seongjin8247@gmail.com>
2022-12-23 18:30:10 +0100

    Add files via upload
    reiniciar juego 그리고 버튼 비활성화 기능 공부

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -13,6 +13,9 @@ function iniciarJuego() {
     botonAgua.addEventListener('click', ataqueAgua)
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 
 function seleccionarMascotaJugador(){
@@ -128,8 +131,19 @@ function crearMensajeFinal(resultadoFinal){
     parrafo.innerHtml = resultadoFinal
 
     sectionMensajes.appendChild(parrafo)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled=true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled=true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled=true
 }
  
+function reiniciarJuego(){
+    location.reload()
+}
+
 function aleatorio(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min)
 }
