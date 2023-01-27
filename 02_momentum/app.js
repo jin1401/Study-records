cb54435e1b1560eb51a4613a5130fe157df970ad
jin1401 <seongjin8247@gmail.com>
2023-01-27 10:38:13 +0100

    Add files via upload
    locatStorage 배움

--- 02_momentum/app.js
+++ 02_momentum/app.js
@@ -6,15 +6,12 @@ const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    loginForm.className.add(HIDDEN_CLASSNAME);
-    console.log(username);    
-    greeting.innerText = "Hello " + username;
+    localStorage.setItem("username", username);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-// hidden 이라는 클래스를 등록해주고 classList.add 또는 classList.remove 속성을 통해 조절해준다.
-// string 을 입력할 떄 `를 활용해서 입력하는 새로운 방식을 배웠다. ' 과 ` 는 다르니 주의한다.
