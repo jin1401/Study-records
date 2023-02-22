6c1b87e600e23b0a3d784781f99c9d47aa093743
Jin <seongjin8247@gmail.com>
2023-02-22 22:54:41 +0100

    Add files via upload
    Learned Class

--- 03_DreamCoding/function.js
+++ 03_DreamCoding/function.js
@@ -146,3 +146,19 @@ const simpleMultiply = (a, b) => {
 // function calculate(commane, a, b)
 // command: add, substract, divide, multiply, remainder
 
+function calculate(command, a, b) {
+    switch (command) {
+        case 'add':
+            return a+b;
+        case 'substract':
+            return a-b;
+        case 'divide':
+            return a/b;
+        case 'multiply':
+            return a*b;
+        case 'remainder':
+            return a%b;
+        default:
+            throw Error('unknown command');
+    }
+}
